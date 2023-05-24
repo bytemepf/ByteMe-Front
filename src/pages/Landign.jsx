b059e560aa5e072d8a6eff4dd4b9f286852059bc
moschi123 <francoeduardo500@gmail.com>
2023-05-23 23:30:07 -0300

    men

--- src/pages/Landign.jsx
+++ src/pages/Landign.jsx
@@ -5,7 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 export default function LandingPage(){
     
     const {loginWithRedirect} = useAuth0();
-    console.log(loginWithRedirect)
+    
     return(
         <div className="main">
             <div className='overlay'></div>
